752fde7c13122aa4f474f173ecd7ea6028d94d7e
Zzozam8898 <Zzozamanimov@gmail.com>
2021-01-07 00:30:54 +0400

    add update file

--- update.lua
+++ update.lua
@@ -1,6 +1,8 @@
   
 local files = {
-    {path = "/home/test/ae2Class.lua", link = "https://raw.githubusercontent.com/rxi/json.lua/master/json.lua"},
+    {path = "/home/test/server.lua", link = "https://raw.githubusercontent.com/PIN-77/ae2manager/merge_branch/server.lua"},
+    {path = "/home/test/ae2Class.lua", link = "https://raw.githubusercontent.com/PIN-77/ae2manager/merge_branch/ae2Class.lua"},
+    {path = "/home/test/tools.lua", link = "https://raw.githubusercontent.com/PIN-77/ae2manager/merge_branch/tools.lua"},
 }
 
 if not filesystem.exists("/home/test") then
