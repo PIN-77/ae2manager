b227e683331d0249919879937c3171589cdf2f78
Zzozam8898 <Zzozamanimov@gmail.com>
2021-01-11 01:11:14 +0400

    a

--- ae2test.lua
+++ ae2test.lua
@@ -7,7 +7,7 @@ local serialization = require('serialization')
 local thread = require('thread')
 local tty = require('tty')
 local unicode = require('unicode')
-
+local GUI = require('GUI')
 -- local inspect = require('inspect')
 
 -- Config --
@@ -35,12 +35,11 @@ local ae2
 -- Functions --
 
 function main()
-    local resetBColor, resetFColor = tty.gpu().getBackground(), tty.gpu().getForeground()
+
 
     initAe2()
     loadRecipes()
     ae2Run(true)
-
 
     -- Start some background tasks
     local background = {}
@@ -49,13 +48,15 @@ function main()
             event.push('exit')
         end
     end))
-    table.insert(background, event.listen("save", saveRecipes))
-    table.insert(background, event.timer(craftingCheckInterval, checkCrafting, math.huge))
-    table.insert(background, thread.create(ae2Loop))
-    --table.insert(background, thread.create(function() app:start() end))
+
+    table.insert(background, event.listen("save", failFast(saveRecipes)))
+    table.insert(background, event.timer(craftingCheckInterval, failFast(checkCrafting), math.huge))
+    table.insert(background, thread.create(failFast(ae2Loop)))
 
     -- Block until we receive the exit signal
     local _, err = event.pull("exit")
+
+
 
     for _, b in ipairs(background) do
         if type(b) == 'table' and b.kill then
@@ -83,12 +84,22 @@ function log(...)
 end
 
 function logRam(msg)
-    free, total = computer.freeMemory(), computer.totalMemory()
-    log(msg, 'RAM', (total - free) * 100 / total, '%')
+    --free, total = computer.freeMemory(), computer.totalMemory()
+    --log(msg, 'RAM', (total - free) * 100 / total, '%')
 end
 
 function pretty(x)
     return serialization.serialize(x, true)
+end
+
+function failFast(fn)
+    return function(...)
+        local res = table.pack(xpcall(fn, debug.traceback, ...))
+        if not res[1] then
+            event.push('exit', res[2])
+        end
+        return table.unpack(res, 2)
+    end
 end
 
 function initAe2()
@@ -99,7 +110,7 @@ function initAe2()
     end
 
     for id, type in pairs(component.list()) do
-        --print('Testing ' .. type .. ' ' .. id)
+        -- print('Testing ' .. type .. ' ' .. id)
         local ok, p = pcall(test_ae2, id)
         if ok then
             print('Component ' .. type .. ' (' .. id .. ') is suitable')
@@ -159,9 +170,9 @@ function ae2Loop()
     while true do
         local e1, e2 = event.pull(fullCheckInterval, 'ae2_loop')
         logRam('loop')
-        log('AE2 loop in')
+        --log('AE2 loop in')
         ae2Run(e2 == 'reload_recipes')
-        log('AE2 loop out')
+        --log('AE2 loop out')
         event.push('redraw')
     end
 end
@@ -170,7 +181,8 @@ end
 function ae2Run(learnNewRecipes)
     local start = computer.uptime()
     updateRecipes(learnNewRecipes)
-    logRam('recipes (post-gc)')
+    logRam('recipes')
+    -- logRam('recipes (post-gc)')
 
     local finder = coroutine.create(findRecipeWork)
     while hasFreeCpu() do
@@ -178,11 +190,10 @@ function ae2Run(learnNewRecipes)
         local _, recipe, needed, craft = coroutine.resume(finder)
         if recipe then
             -- Request crafting
-            log('needed'..needed)
-            log(maxBatch)
             local amount = math.min(needed, maxBatch)
-            log('Requesting ' .. amount .. ' ' .. recipe.label)
+            --log('Requesting ' .. amount .. ' ' .. recipe.label)
             recipe.crafting = craft.request(amount)
+            yield('yield crafting')
             checkFuture(recipe) -- might fail very quickly (missing resource, ...)
         else
             break
@@ -203,6 +214,12 @@ function checkCrafting()
     end
 end
 
+function yield(msg)
+    --local gpu = tty.gpu()
+    --local _, h = gpu.getViewport()
+    --gpu.set(1, h, msg)
+    os.sleep()
+end
 
 function updateRecipes(learnNewRecipes)
     local start = computer.uptime()
@@ -344,6 +361,40 @@ function checkFuture(recipe)
     return false
 end
 
+function equals(t1, t2)
+    if t1 == t2 then return true end
+    if type(t1) ~= type(t2) or type(t1) ~= 'table' then return false end
+
+    for k1, v1 in pairs(t1) do
+        local v2 = t2[k1]
+        if not equals(v1, v2) then return false end
+    end
+
+    for k2, _ in pairs(t2) do
+        if t1[k2] == nil then return false end
+    end
+
+    return true
+end
+
+function filter(array, predicate)
+    local res = {}
+    for _, v in ipairs(array) do
+        if predicate(v) then table.insert(res, v) end
+    end
+    return res
+end
+
+function contains(haystack, needle)
+    if haystack == needle then return true end
+    if type(haystack) ~= type(needle) or type(haystack) ~= 'table' then return false end
+
+    for k, v in pairs(needle) do
+        if not contains(haystack[k], v) then return false end
+    end
+
+    return true
+end
 
 function hasFreeCpu()
     local cpus = ae2.getCpus()
@@ -389,7 +440,7 @@ function findRecipeWork() --> yield (recipe, needed, craft)
 
         yield('yield '..i)
         local craftables, err = ae2.getCraftables(recipe.item)
-        log('get_craftable', inspect(craftables))
+        --log('get_craftable', inspect(craftables))
         if err then
             recipe.error = 'ae2.getCraftables ' .. tostring(err)
         elseif #craftables == 0 then
@@ -402,6 +453,224 @@ function findRecipeWork() --> yield (recipe, needed, craft)
 
         ::continue::
     end
+end
+
+function override(object, method, fn)
+    local super = object[method] or function() end
+    object[method] = function(...)
+        fn(super, ...)
+    end
+end
+
+function numberValidator(str)
+    n = tonumber(str, 10)
+    return n and math.floor(n) == n
+end
+
+-- Stay close to the 16 Minecraft colors in order to work on gold GPU/screen
+local C_BACKGROUND = 0x0F2231 -- фон
+local C_STATUS_BAR = 0x0F2231 -- фон пбс
+local C_STATUS_TEXT = 0xC4D0E0 -- текст
+local C_STATUS_PRESSED = 0xFF0000 -- активный текст
+local C_BADGE = 0x0F2231 -- фон скроллбара через одну
+local C_BADGE_ERR = 0x445565 -- ошибка элементов
+local C_BADGE_BUSY = 0x445565 -- корректные элементов
+local C_BADGE_SELECTED = 0x445565 -- выделение элементов скроллбара
+local C_BADGE_TEXT = 0xC4D0E0 -- цвет текста
+local C_INPUT = 0xFFFFFF -- поиск
+local C_INPUT_TEXT = 0x1E1E1E
+local C_SCROLLBAR = 0x99CCBB -- скролл полоска
+local C_SCROLLBAR_BACKGROUND = 0xFFFFFF -- фон скролбара
+
+function buildGui()
+    local app = GUI.application()
+    local statusBar = app:addChild(GUI.container(1, 1, app.width, 1))
+    local window = app:addChild(GUI.container(1, 1 + statusBar.height, app.width, app.height - statusBar.height))
+
+    window:addChild(GUI.panel(1, 1, window.width, window.height, C_BACKGROUND))
+    local columns = math.floor(window.width / 60) + 1
+
+    -- Crating queue view
+    local craftingQueueView = window:addChild(GUI.layout(1, 1, window.width-1, window.height, columns, 1))
+    for i = 1, columns do
+        craftingQueueView:setAlignment(i, 1, GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_TOP)
+        craftingQueueView:setMargin(i, 1, .5, 1)
+    end
+
+    override(craftingQueueView, 'draw', function(super, self, ...)
+        self.children = {}
+
+        local added = 0
+        for _, recipe in ipairs(recipes) do
+            local color =
+            recipe.error and C_BADGE_ERR or
+                    recipe.crafting and C_BADGE_BUSY or
+                    (recipe.stored or 0) < recipe.wanted and C_BADGE
+
+            if color then
+                local badge = GUI.container(1, 1, math.floor(self.width / columns - 1), 4)
+                self:setPosition(1 + added % columns, 1, self:addChild(badge))
+                badge:addChild(GUI.panel(1, 1, badge.width, 4, color))
+                badge:addChild(GUI.text(2, 2, C_BADGE_TEXT, recipe.label)) -- TODO: include the item icon ?
+                badge:addChild(GUI.text(2, 3, C_BADGE_TEXT, string.format('%s / %s', recipe.stored or '?', recipe.wanted)))
+                if recipe.error then
+                    badge:addChild(GUI.text(2, 4, C_BADGE_TEXT, tostring(recipe.error)))
+                    badge:moveToFront()
+                end
+
+                added = added + 1
+            end
+        end
+
+        super(self, ...)
+    end)
+
+    -- Configuration view
+    local SYMBOL_CONFIG_RECIPE = {}
+    local configView = window:addChild(GUI.container(1, 1, window.width, window.height))
+    configView:addChild(GUI.panel(1, 1, configView.width, configView.height, C_BACKGROUND))
+    configView.hidden = true
+
+    -- left panel (item select)
+    local itemListSearch = configView:addChild(GUI.input(2, 2, configView.width/2-1, 3,
+            C_INPUT, C_INPUT_TEXT, C_INPUT_TEXT, C_STATUS_PRESSED, C_INPUT_TEXT, '', 'Поиск'))
+
+    -- TODO: add unconfigured/hidden filter
+
+    local itemListPanel = configView:addChild(GUI.list(
+            itemListSearch.x, itemListSearch.y + itemListSearch.height + 1, itemListSearch.width, configView.height-itemListSearch.height-3,
+            1, 0, C_BADGE, C_BADGE_TEXT, C_STATUS_BAR, C_STATUS_TEXT, C_BADGE_SELECTED, C_BADGE_TEXT
+    ))
+    itemListPanel.selectedItem = -1
+    --itemListPanel:setAlignment(GUI.ALIGNMENT_HORIZONTAL_LEFT, GUI.ALIGNMENT_VERTICAL_TOP)
+    attachScrollbar(itemListPanel)
+
+    override(itemListPanel, 'draw', function (super, self, ...)
+        self.selectedItem = -1
+        self.children = {}
+
+        local selection = recipes
+        local filter = itemListSearch.text
+        if filter and filter ~= '' then
+            filter = unicode.lower(filter)
+            selection = {}
+            for _, recipe in ipairs(recipes) do
+                -- Patterns seem very limited, no case-insensitive option
+                if unicode.lower(recipe.label):find(filter) then
+                    table.insert(selection, recipe)
+                end
+            end
+        end
+
+        self.scrollBar.maximumValue = math.max(0, #selection - self.height)
+        self.scrollBar.shownValueCount =  self.scrollBar.maximumValue / (self.scrollBar.maximumValue + 1)
+
+        local offset = self.scrollBar.value
+        for i = 1, math.min(self.height, #selection) do
+            local recipe = selection[offset + i]
+            local choice = self:addItem(recipe.label)
+            --choice.colors.default.background = (recipe.error ~= nil) and C_BADGE_ERR or recipe.wanted > 0 and C_BADGE_BUSY or C_BADGE
+            if recipe == configView[SYMBOL_CONFIG_RECIPE] then
+                self.selectedItem = i
+            end
+            choice.onTouch = function(app, object)
+                configView[SYMBOL_CONFIG_RECIPE] = recipe
+                event.push('config_recipe_change')
+            end
+        end
+
+        super(self, ...)
+    end)
+
+    -- right panel (item details)
+    local reloadBtn = configView:addChild(GUI.button(configView.width/2+2, 2, configView.width/2-2, 3,
+                                                     C_BADGE, C_BADGE_TEXT, C_BADGE, C_STATUS_PRESSED, "Перезагрузка рецептов"))
+    reloadBtn.onTouch = function(app, self)
+        event.push('ae2_loop', 'reload_recipes')
+    end
+    local itemConfigPanel = configView:addChild(GUI.layout(reloadBtn.x, reloadBtn.y + reloadBtn.height + 1, reloadBtn.width, configView.height-reloadBtn.height-7, 1, 1))
+    configView:addChild(GUI.panel(itemConfigPanel.x, itemConfigPanel.y, itemConfigPanel.width, itemConfigPanel.height, C_BADGE)):moveBackward()
+    itemConfigPanel:setAlignment(1, 1, GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_TOP)
+    itemConfigPanel:setMargin(1, 1, .5, 1)
+
+    override(itemConfigPanel, 'eventHandler', function(super, app, self, key, ...)
+        if key == "config_recipe_change" then
+            local recipe = configView[SYMBOL_CONFIG_RECIPE]
+
+            self.children = {}
+            self:addChild(GUI.text(1, 1, C_BADGE_TEXT, '[ '..recipe.label..' ]'))
+            self:addChild(GUI.text(1, 1, C_BADGE_TEXT, "Хранится: "..tostring(recipe.stored)))
+            self:addChild(GUI.text(1, 1, C_BADGE_TEXT, "Поддерживать"))
+            local wantedInput = self:addChild(GUI.input(1, 1, 10, 3,
+                    C_INPUT, C_INPUT_TEXT, 0, C_STATUS_PRESSED, C_INPUT_TEXT, tostring(recipe.wanted)))
+            wantedInput.validator = numberValidator
+            wantedInput.onInputFinished = function(app, object)
+                recipe.wanted = tonumber(object.text) or error('cannot parse '..object.text)
+                event.push('ae2_loop')
+                event.push('save')
+            end
+
+            -- TODO: add remove/hide option
+
+            -- self:draw()
+            event.push('redraw') -- There is probably a more elegant way to do it ¯\_(ツ)_/¯
+        end
+        super(app, self, key, ...)
+    end)
+
+    local resetRecipeBtn = configView:addChild(GUI.button(itemConfigPanel.x, itemConfigPanel.y + itemConfigPanel.height + 1, itemConfigPanel.width, 3,
+                                                          C_BADGE, C_BADGE_TEXT, C_BADGE, C_STATUS_PRESSED, "Не нажимать, будет cum. Русифицировано ananaslox ОнЖе AnalAnus"))
+    resetRecipeBtn.onTouch = function(app, self)
+        local recipe = configView[SYMBOL_CONFIG_RECIPE]
+        if not recipe then return end
+        for i, candidate in ipairs(recipes) do
+            if (candidate == recipe) then
+                table.remove(recipes, i)
+                return
+            end
+        end
+    end
+
+    -- Status bar
+    statusBar:addChild(GUI.panel(1, 1, statusBar.width, statusBar.height, C_STATUS_BAR))
+    local statusText = statusBar:addChild(GUI.text(2, 1, C_STATUS_TEXT, ''))
+    statusText.eventHandler = function(app, self)
+        self.text = string.format('%d процессоров свободно из %d.  %d ошибок, %d текущее, %d запланировано.  Задержка: %.0f ms.',
+            status.cpu.free, status.cpu.all, status.recipes.error, status.recipes.crafting, status.recipes.queue, status.update.duration * 1000)
+    end
+    statusText.eventHandler(app, statusText)
+    local cfgBtn = statusBar:addChild(GUI.button(statusBar.width - 16, 1, 8, 1, C_STATUS_BAR, C_STATUS_TEXT, C_STATUS_BAR, C_STATUS_PRESSED, '[Настройки]'))
+    cfgBtn.switchMode = true
+    cfgBtn.animationDuration = .1
+    cfgBtn.onTouch = function(app, object)
+        configView.hidden = not object.pressed
+    end
+    statusBar:addChild(GUI.button(statusBar.width - 6, 1, 8, 1, C_STATUS_BAR, C_STATUS_TEXT, C_STATUS_BAR, C_STATUS_PRESSED, '[Выход]')).onTouch = function(app, object)
+        event.push('exit')
+    end
+	statusBar:addChild(GUI.button(statusBar.width - 30, 1, 8, 1, C_STATUS_BAR, C_STATUS_TEXT, C_STATUS_BAR, C_STATUS_PRESSED, '[Перезагрузка]')).onTouch = function(app, object)
+        computer.shutdown(true)
+    end
+	
+    return app
+end
+
+function attachScrollbar(obj)
+    local width = (obj.width > 60) and 2 or 1
+    obj.width = obj.width - width
+    local bar = GUI.scrollBar(obj.x+obj.width, obj.y, width, obj.height, C_SCROLLBAR_BACKGROUND, C_SCROLLBAR,
+            0, 1, 0, 1, 4, false)
+    obj.parent:addChild(bar)
+    obj.scrollBar = bar
+
+    override(obj, 'eventHandler', function (super, app, self, key, ...)
+        if key == 'scroll' then -- forward scrolls on the main object to the scrollbar
+            bar.eventHandler(app, bar, key, ...)
+        end
+        super(app, self, key, ...)
+    end)
+
+    return bar
 end
 
 -- Start the program
