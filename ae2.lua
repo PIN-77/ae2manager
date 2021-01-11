2ead868c9fd89310c07a2c3950454d6b5e605018
PIN-77 <evgeniy202829@gmail.com>
2021-01-11 16:10:53 +0600

    as

--- ae2.lua
+++ ae2.lua
@@ -1,10 +1,8 @@
 local GUI = require('GUI')
 
-function buildGui()
     local application = GUI.application()
     application:addChild(GUI.container(1, 1, application.width, application.height, 0x002440))
     application:addChild(GUI.panel(10, 10, application.width - 20, application.height - 20, 0x880000))
-end
 
 application:draw(true)
 application:start()
